f070198d78f4040d6b7215f58cd0c949def0ea31
Nicolas <n_skatchkovsky@msn.com>
2020-11-03 16:06:00 +0000

    implementing

--- train_decolle_lenet_bbsnn.py
+++ train_decolle_lenet_bbsnn.py
@@ -37,6 +37,9 @@ if __name__ == "__main__":
     parser.add_argument('--save_path', type=str, default=None, help='Path to where weights are stored (relative to home)')
     parser.add_argument('--n_epochs', type=int, default=1000)
     parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--temperature', type=float, default=0.1)
+    parser.add_argument('--rho', type=float, default=5e-7)
+    parser.add_argument('--prior_p', type=float, default=0.5)
     parser.add_argument('--disable-cuda', type=str, default='false', help='Disable CUDA')
 
     args = parser.parse_args()
