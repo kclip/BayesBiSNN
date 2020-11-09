ca8617f41761734e82397c11b0032d5e3f3073d1
Nicolas <n_skatchkovsky@msn.com>
2020-11-09 10:54:09 +0100

    implementing

--- twomoons_decolle_mlp_stlr.py
+++ twomoons_decolle_mlp_stlr.py
@@ -29,7 +29,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train probabilistic multivalued SNNs using Pytorch')
 
     # Training arguments
-    parser.add_argument('--home', default='/home')
+    parser.add_argument('--home', default=r"C:\Users\K1804053\OneDrive - King's College London\PycharmProjects")
+    parser.add_argument('--results', default=r"C:\Users\K1804053\results")
     parser.add_argument('--save_path', type=str, default=None, help='Path to where weights are stored (relative to home)')
     parser.add_argument('--n_epochs', type=int, default=3000)
     parser.add_argument('--lr', type=float, default=5e-2)
@@ -37,14 +38,14 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-pre = args.home + r'/results/'
-prelist = np.sort(fnmatch.filter(os.listdir(pre), '[0-9][0-9][0-9]__*'))
+prelist = np.sort(fnmatch.filter(os.listdir(args.results), '[0-9][0-9][0-9]__*'))
 if len(prelist) == 0:
     expDirN = "001"
 else:
     expDirN = "%03d" % (int((prelist[len(prelist) - 1].split("__"))[0]) + 1)
 
-results_path = time.strftime(pre + expDirN + "__" + "%d-%m-%Y", time.localtime()) + '_' + 'mnist_dvs_stlr' + r'_%d_epochs' % args.n_epochs
+results_path = time.strftime(args.results + r'/' + expDirN + "__" + "%d-%m-%Y",
+                             time.localtime()) + '_' + 'mnist_dvs_stlr' + r'_%d_epochs' % args.n_epochs + '_lr_%f' % args.lr
 os.makedirs(results_path)
 
 args.disable_cuda = str2bool(args.disable_cuda)
@@ -128,7 +129,7 @@ for epoch in range(args.n_epochs):
         inputs = x_bin_train[idxs].permute(1, 0, 2).to(args.device)
         labels = y_bin_train[idxs].permute(0, 2, 1).to(args.device)
 
-        print([Counter(w.detach().numpy().flatten()) for w in binary_model.parameters()])
+        # print([Counter(w.detach().numpy().flatten()) for w in binary_model.parameters()])
         binary_model.init(inputs, burnin=burnin)
 
         readout_hist = [torch.Tensor() for _ in range(len(binary_model.readout_layers))]
@@ -160,8 +161,6 @@ for epoch in range(args.n_epochs):
     # print(true_labels)
     # print(preds)
     acc = torch.sum(preds == true_labels).float() / n_samples_train
-    np.save(os.path.join(results_path, 'train_predictions_latest'), preds.numpy())
-    np.save(os.path.join(results_path, 'idxs_train'), np.array(idxs_used))
 
     # backward pass: compute gradient of the loss with respect to model parameters
     print(acc)
@@ -203,3 +202,41 @@ for epoch in range(args.n_epochs):
 
             np.save(os.path.join(results_path, 'test_predictions_latest'), predictions.numpy())
             np.save(os.path.join(results_path, 'idxs_test'), np.array(idxs_used_test))
+            
+            
+            
+            n_batchs_train = n_samples_train // batch_size + (1 - (n_samples_train % batch_size == 0))
+            idx_avail_train = np.arange(n_samples_train)
+            idxs_used_train = []
+
+            print('Mode training epoch %d/%d' % (epoch + 1, args.n_epochs))
+            predictions = torch.FloatTensor()
+
+            for i in tqdm(range(n_batchs_train)):
+                if (i == (n_batchs_train - 1)) & (n_samples_train % batch_size != 0):
+                    batch_size_curr = n_samples_train % batch_size
+                else:
+                    batch_size_curr = batch_size
+
+                idxs_train = np.random.choice(idx_avail_train, [batch_size_curr], replace=False)
+                idxs_used_train += list(idxs_train)
+                idx_avail_train = [i for i in idx_avail_train if i not in idxs_used_train]
+
+                inputs = x_bin_train[idxs_train].permute(1, 0, 2).to(args.device)
+
+                binary_model.init(inputs, burnin=burnin)
+
+                readout_hist = [torch.Tensor() for _ in range(len(binary_model.readout_layers))]
+
+                for t in range(burnin, T):
+                    # forward pass: compute predicted outputs by passing inputs to the model
+                    s, r, u = binary_model(inputs[t])
+
+                    for l, ro_h in enumerate(readout_hist):
+                        readout_hist[l] = torch.cat((ro_h, r[l].cpu().unsqueeze(0)), dim=0)
+
+                predictions = torch.cat((predictions, torch.sum(readout_hist[-1], dim=0)))
+
+            np.save(os.path.join(results_path, 'train_predictions_latest'), predictions.numpy())
+            np.save(os.path.join(results_path, 'idxs_train'), np.array(idxs_used_train))
+
