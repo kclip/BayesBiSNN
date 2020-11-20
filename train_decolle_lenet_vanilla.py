4b859c0a339555e600e73e71a6424f7f404aa4e4
Nicolas <n_skatchkovsky@msn.com>
2020-11-20 14:23:00 +0100

    implementing

--- train_decolle_lenet_vanilla.py
+++ train_decolle_lenet_vanilla.py
@@ -138,8 +138,8 @@ for epoch in range(args.n_epochs):
         # calculate the loss
         loss = decolle_loss(s, r, u, target=labels[:, :, t])
 
-        print([torch.max(w.grad) for w in model.get_trainable_parameters()])
-        print([torch.max(w) for w in model.get_trainable_parameters()])
+        print([torch.max(w.grad) for w in model.get_trainable_parameters() if w is not None])
+        print([torch.max(w) for w in model.get_trainable_parameters() if w is not None])
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
