209c9161c99fe64a46e222ded6c9fe6ac3e6f0b8
Nicolas <n_skatchkovsky@msn.com>
2020-12-07 11:08:07 +0100

    implementing

--- search_decolle_lenet_stlr.py
+++ search_decolle_lenet_stlr.py
@@ -2,7 +2,7 @@ import torch
 from utils.loss import DECOLLELoss, one_hot_crossentropy
 from model.LeNet import LenetLIF
 from tqdm import tqdm
-from optimizer.BBSNN import BayesBiSNNRP
+from optimizer.BiSGD import BiSGD
 from copy import deepcopy
 from utils.binarize import binarize, binarize_stochastic
 import os
@@ -48,7 +48,7 @@ else:
     expDirN = "%03d" % (int((prelist[len(prelist) - 1].split("__"))[0]) + 1)
 
 results_path = time.strftime(args.results + r'/' + expDirN + "__" + "%d-%m-%Y",
-                             time.localtime()) + '_' + 'search_mnist_dvs_bbsnnrp_lenet'
+                             time.localtime()) + '_' + 'search_mnist_dvs_stlr_lenet'
 os.makedirs(results_path)
 
 args.polarity = str2bool(args.polarity)
@@ -89,7 +89,6 @@ lr_list = np.logspace(-5, 8, 14, endpoint=True)
 rho_list = np.logspace(-14, -3, 12, endpoint=True)
 
 results_l1 = {i: {j: [] for j in rho_list} for i in lr_list}
-results_l2 = {i: {j: [] for j in rho_list} for i in lr_list}
 
 for lr in lr_list:
     for rho in rho_list:
@@ -115,7 +114,7 @@ for lr in lr_list:
         decolle_loss = DECOLLELoss(criterion, latent_model)
 
         # specify optimizer
-        optimizer = BayesBiSNNRP(binary_model.parameters(), latent_model.parameters(), lr=lr, temperature=args.temperature, prior_p=args.prior_p, rho=rho, device=args.device)
+        optimizer = BiSGD(binary_model.parameters(), latent_model.parameters(), lr=args.lr, binarizer=binarize)
 
         binary_model.init_parameters()
 
@@ -129,20 +128,26 @@ for lr in lr_list:
             inputs = inputs.transpose(0, 1).to(args.device)
             labels = labels.to(args.device)
 
-            optimizer.update_concrete_weights()
             binary_model.init(inputs, burnin=burnin)
 
-            readout_hist = train_on_example_bbsnn(binary_model, optimizer, decolle_loss, inputs, labels, burnin, T)
+            readout_hist = [torch.Tensor() for _ in range(len(binary_model.readout_layers))]
+
+            for t in range(burnin, T):
+                # forward pass: compute predicted outputs by passing inputs to the model
+                s, r, u = binary_model(inputs[t])
+
+                for l, ro_h in enumerate(readout_hist):
+                    readout_hist[l] = torch.cat((ro_h, r[l].cpu().unsqueeze(0)), dim=0)
+
+                # calculate the loss
+                loss = decolle_loss(s, r, u, target=labels[:, :, t])
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
             acc = get_acc(torch.sum(readout_hist[-1], dim=0).argmax(dim=1), labels, args.batch_size)
             results_l1[lr][rho].append(acc)
             print(acc)
-            acc = get_acc(torch.sum(readout_hist[-2], dim=0).argmax(dim=1), labels, args.batch_size)
-            print(acc)
-            results_l2[lr][rho].append(acc)
 
         with open(results_path + '/res_l1.pkl', 'wb') as f:
             pickle.dump(results_l1, f, pickle.HIGHEST_PROTOCOL)
-
-        with open(results_path + '/res_l2.pkl', 'wb') as f:
-            pickle.dump(results_l2, f, pickle.HIGHEST_PROTOCOL)
-
